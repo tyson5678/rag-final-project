dee235d853cf4c54882ab60b72a6dc9d7acaeb2c
Kai-Hsun Chien <t1102b0009@ntut.org.tw>
2025-12-05 16:48:07 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_chroma import Chroma
-from langchain.chains import create_retrieval_chain
+from langchain.chains.retrieval import create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_core.prompts import ChatPromptTemplate
 
